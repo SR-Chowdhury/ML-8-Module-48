0943fb2e0a045f1f0e68d1fcd7344f2909041546
Shihan Chowdhury <hackedgdown@gmail.com>
2023-03-24 21:38:59 +0000

    Surge also done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='text-center my-5'>
+            <h1>Bismillahir Rahmanir Rahim</h1>
+        </div>
+    );
+};
+
+export default Header;
